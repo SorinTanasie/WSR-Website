e9276d7de9caf01d9a189256b2c40aa1352b784f
mr robot <geicarobert@gmail.com>
2020-05-10 13:02:28 +0300

    preloader, portofolio page update

--- src/App.js
+++ src/App.js
@@ -12,24 +12,32 @@ import Footer from './components/footer/footer.component';
 import PortoPage from './pages/portofolio/portofolio.component';
 import AboutUs from './pages/about-us/about-us.component';
 
+import { Circle2 } from 'react-preloaders';
+
 
 function App() {
   return (
-    <Router>
-      <Navbar />
-      
-      <Switch>
+    <div>
+      <Router>
+        <Navbar />
+        
+        <Switch>
 
-        <Route exact path="/despre-noi" component={AboutUs} />
+          <Route exact path="/despre-noi" component={AboutUs} />
 
-        <Route exact path="/portofoliu" component={PortoPage} />
+          <Route exact path="/portofoliu" component={PortoPage} />
 
-        <Route  path="/" component={Homepage} />
+          <Route  path="/" component={Homepage} />
 
 
-      </Switch>
-      <Footer />
-    </Router>
+        </Switch>
+        <Footer />
+      </Router>
+
+      <Circle2 color={'#314584'} />;
+
+    </div>
+
   );
 }
 
--- src/pages/about-us/about-us.component.jsx
+++ src/pages/about-us/about-us.component.jsx
@@ -2,24 +2,27 @@ import React from 'react';
 import './about-us.styles.scss';
 import AboutImg from '../../components/about-us/AboutImg';
 import AboutText from '../../components/about-us/AboutText';
-
+import ContactForm from '../../components/form/form.component';
 import logo from '../../Assets/team1.png';
 
 const AboutUs = () => {
     return (
-        <div>
+        <div className="aboutus-page">
             <div className="about">
-                <AboutImg logo={logo} />
                 <AboutText 
                     smallText="some text here"
                     title="this is the title"
                     firstP="a paragraph, a paragraph, a paragraph, a paragraph, a paragraph, a paragraph"
-                    secondP="two paragraphs, two paragraphs, two paragraphs, two paragraphs, two paragraphs, two paragraphs, two paragraphs, two paragraphs, two paragraphs, two paragraphs, two paragraphs, two paragraphs, two paragraphs, two paragraphs, two paragraphs, two paragraphs, two paragraphs, two paragraphs, two paragraphs, two paragraphs"
+                    secondP="two paragraphs, two paragraphs, two paragraphs, two paragraphs, two paragraphs, two paragraphs, two paragraphs, two paragraphs, two paragraphs, two paragraphs, two paragraphs"
                     thirdP="or more, or more, or more, or more, or more, or more, or more, or more, or more, or more, or more, or more, or more"
                 />
+                <AboutImg logo={logo} />
+
             </div>
 
 
+
+                <ContactForm />
         </div>
 
     )
